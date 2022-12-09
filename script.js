2a4f659475e1dd824b349ec0780c5085414f0fbb
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2022-12-09 20:42:36 +0100

    Cleaned up code a little bit

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+let computerWinsCount = 0;
+let playerWinsCount = 0;
+let playedRoundsCount = 0;
+
 function getComputerChoice(){
+
+     //Because of Math.round anything between 3 and 3.4 will result in 3
     let choice = Math.round(Math.random(3.4));
     
     switch(choice){
+
         case 0:
              return "rock";
         case 1:
@@ -14,106 +21,139 @@ function getComputerChoice(){
     }
 
 }
-let computerWins = 0;
-let playerWins = 0;
-let playedRounds = 0;
+
 function playWithMe(){
-while(playedRounds != 5){
+
+   while(playedRoundsCount != 5){
+
      playGame()
-}
+
+   }
 }
 
-    function playGame(){
-    let compchoice = getComputerChoice();
-    let userchoice = window.prompt("Chose one: Rock, Paper, Scissors").toLowerCase();
-    console.log("Computer chose --- " + compchoice);
-    switch(userchoice){
+function playGame(){
+
+    let compChoice = getComputerChoice();
+    let userChoice = window.prompt("Chose one: Rock, Paper, Scissors").toLowerCase();
+    console.log("Computer chose --- " + compChoice);
+
+    switch(userChoice){
+
         case "rock":
-            if(compchoice == userchoice){
-                 alert("Two rocks are rock solid, it's a draw!");
-                 alert("Player: " + playerWins + " | Computer: " + computerWins);
-                 alert("Round: " + (playedRounds + 1) + " played!");
-                  return playedRounds += 1;
+
+          if(compChoice == userChoice){
+
+               alert("Two rocks are rock solid, it's a draw!");
+               alert("Player: " + playerWinsCount + " | Computer: " + computerWinsCount);
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return playedRoundsCount += 1;
 
             }
-            else if(compchoice == "paper"){
-                computerWins += 1;
-                 alert("I wraped you up, I win!")
-                 alert("Player: " + playerWins + " | Computer: " + (computerWins + 1));
-                 alert("Round: " + (playedRounds + 1) + " played!");
-                 return (playedRounds += 1, computerWins += 1);
-            }
-            else {
-                
-                 alert("You broke me cause you are harder then steel, I lose!")
-                 alert("Player: " + (playerWins + 1) + " | Computer: " + computerWins);
-                 alert("Round: " + (playedRounds + 1) + " played!");
-                 return (playerWins += 1, playedRounds += 1);
-            }
-        case "paper":
-        if(compchoice == userchoice){
-             alert("That's a lot of papaer, maybe we could write something down... It's a draw!")
-             alert("Player: " + playerWins + " | Computer: " + computerWins);
-                 alert("Round: " + (playedRounds + 1) + " played!");
-             return playedRounds += 1;
-            }
-        else if(compchoice == "scissors"){
-           
-             alert("I cut you up, I win!");
-             alert("Player: " + playerWins + " | Computer: " + (computerWins + 1));
-                 alert("Round: " + (playedRounds + 1) + " played!");
-             return  (playedRounds += 1,computerWins += 1);
-        }
-        else {
             
-             alert("You wraped around me, I lose!");
-             alert("Player: " + (playerWins + 1) + " | Computer: " + computerWins);
-                 alert("Round: " + (playedRounds + 1) + " played!");
-            return (playerWins += 1, playedRounds += 1);
-        }
-        case "scissors":
-            if(compchoice == userchoice){
-                 alert("You're weirdo... It's a draw!");
-                 alert("Player: " + playerWins + " | Computer: " + computerWins);
-                 alert("Round: " + (playedRounds + 1) + " played!");
-                 return(playedRounds += 1);
+          else if(compChoice == "paper"){
+
+               computerWinsCount += 1;
+               alert("I wraped you up, I win!")
+               alert("Player: " + playerWinsCount + " | Computer: " + (computerWinsCount + 1));
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return (playedRoundsCount += 1, computerWinsCount += 1);
+
             }
-            else if(compchoice == "rock"){
-                    
-                      alert("I break you cause I am harder then steel, I win!");
-                      alert("Player: " + playerWins + " | Computer: " + (computerWins + 1));
-                 alert("Round: " + (playedRounds + 1) + " played!");
-            return (computerWins += 1, playedRounds += 1);
+
+          else {
+                
+               alert("You broke me cause you are harder then steel, I lose!")
+               alert("Player: " + (playerWinsCount + 1) + " | Computer: " + computerWinsCount);
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return (playerWinsCount += 1, playedRoundsCount += 1);
+
+            }
+
+        case "paper":
+
+          if(compChoice == userChoice){
+
+               alert("That's a lot of papaer, maybe we could write something down... It's a draw!")
+               alert("Player: " + playerWinsCount + " | Computer: " + computerWinsCount);
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return playedRoundsCount += 1;
+
+            }
+
+          else if(compChoice == "scissors"){
+           
+               alert("I cut you up, I win!");
+               alert("Player: " + playerWinsCount + " | Computer: " + (computerWinsCount + 1));
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return (playedRoundsCount += 1,computerWinsCount += 1);
+
         }
-            else {
+
+          else {
+            
+               alert("You wraped around me, I lose!");
+               alert("Player: " + (playerWinsCount + 1) + " | Computer: " + computerWinsCount);
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return (playerWinsCount += 1, playedRoundsCount += 1);
+
+        }
+
+        case "scissors":
+
+          if(compChoice == userChoice){
+
+               alert("You're weirdo... It's a draw!");
+               alert("Player: " + playerWinsCount + " | Computer: " + computerWinsCount);
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return (playedRoundsCount += 1);
+
+            }
+
+          else if(compChoice == "rock"){
+                    
+               alert("I break you cause I am harder then steel, I win!");
+               alert("Player: " + playerWinsCount + " | Computer: " + (computerWinsCount + 1));
+               alert("Round: " + (playedRoundsCount + 1) + " played!");
+               return (computerWinsCount += 1, playedRoundsCount += 1);
+
+        }
+
+          else {
                 
                  alert("You cut me up, you win!");
-                 alert("Player: " + (playerWins + 1) + " | Computer: " + computerWins);
-                 alert("Round: " + (playedRounds + 1) + " played!");
-                return (playerWins += 1, playedRounds += 1);
+                 alert("Player: " + (playerWinsCount + 1) + " | Computer: " + computerWinsCount);
+                 alert("Round: " + (playedRoundsCount + 1) + " played!");
+                 return (playerWinsCount += 1, playedRoundsCount += 1);
+
             }
+
         default:
+
             alert("Check your spelling you fool!");
             playGame();
 
     }
 }
 function showResult(){
-    if(computerWins > playerWins){
+
+          if(computerWinsCount > playerWinsCount){
         
-        
-     alert("I win because you are unluckiest person alive!");
-}
-else if(computerWins == playerWins){
+               alert("I win because you are unluckiest person alive!");
+
+     }
+
+          else if(computerWinsCount == playerWinsCount){
+   
+               alert("I'll see you again, but next time will be last time we see each other!");
+
+     }
+
+          else {
     
-     
-    alert("I'll see you again, but next time will be last time we see each other!");
-}
-else {
-    
-      
-     alert("You were lucky! You win!")
-}
+               alert("You were lucky! You win!")
+
+     }
+
 }
 
 
